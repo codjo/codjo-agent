eecc558aa68ba8adba5b61010421e9e17d42eefc
Fabien DUMINY <fabien.duminy@allianzgi.fr>
2013-08-23 16:17:26 +0200

    - moves ListAppender class from codjo-agent to codjo-test - uses LoggerRule for checking log4j's messages, instead of using directly ListAppender

--- src/test/java/net/codjo/agent/test/AgentContainerFixtureTest.java
+++ src/test/java/net/codjo/agent/test/AgentContainerFixtureTest.java
@@ -1,17 +1,27 @@
 package net.codjo.agent.test;
 import junit.framework.AssertionFailedError;
-import junit.framework.TestCase;
 import net.codjo.test.common.LogString;
+import net.codjo.test.common.LoggerRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 /**
  *
  */
-public class AgentContainerFixtureTest extends TestCase {
+public class AgentContainerFixtureTest {
     private static final int MAX_TRY_BEFORE_FAILURE = 4;
     private static final int ASSERT_TIMEOUT = 1;
     private LogString log = new LogString();
     AgentContainerFixture fixture = new AgentContainerFixture();
 
+    @Rule
+    public final LoggerRule loggerRule = new LoggerRule();
 
+
+    @Test
     public void test_assertUntilOk() throws Exception {
         fixture.assertUntilOk(new AgentAssert.Assertion() {
             int index = 0;
@@ -29,44 +39,34 @@ public class AgentContainerFixtureTest extends TestCase {
     }
 
 
+    @Test
     public void test_assertUntilOk_failure() throws Exception {
-        final ListAppender logs = ListAppender.createAndAddToRootLogger();
-
         try {
-            try {
-                fixture.assertUntilOk(new AgentAssert.Assertion() {
-                    int index = 0;
+            fixture.assertUntilOk(new AgentAssert.Assertion() {
+                int index = 0;
 
 
-                    public void check() throws Throwable {
-                        index++;
-                        log.call("check", index);
-                        throw new NullPointerException("erreur");
-                    }
-                });
-                throw new Error("Doit echouer");
-            }
-            catch (AssertionFailedError ex) {
-                assertEquals("erreur", ex.getLocalizedMessage());
-                assertTrue(ex.getCause() instanceof NullPointerException);
-            }
-            log.assertContent("check(1), check(2), check(3), check(4)");
-            assertTrue("nbTries logged", logs.matchesOneLine(
-                  "ERROR: \n!!!\n!!! AgentAssert failed after " + MAX_TRY_BEFORE_FAILURE + " tries, with a timeout of "
-                  + ASSERT_TIMEOUT + " ms per try\n!!!"));
+                public void check() throws Throwable {
+                    index++;
+                    log.call("check", index);
+                    throw new NullPointerException("erreur");
+                }
+            });
+            throw new Error("Doit echouer");
         }
-        catch (AssertionFailedError afe) {
-            logs.printTo(System.out);
-            throw afe;
+        catch (AssertionFailedError ex) {
+            assertEquals("erreur", ex.getLocalizedMessage());
+            assertTrue(ex.getCause() instanceof NullPointerException);
         }
-        finally {
-            logs.removeFromRootLogger();
-        }
+        log.assertContent("check(1), check(2), check(3), check(4)");
+        assertTrue("nbTries logged", loggerRule.getAppender().matchesOneLine(
+              "ERROR: \n!!!\n!!! AgentAssert failed after " + MAX_TRY_BEFORE_FAILURE + " tries, with a timeout of "
+              + ASSERT_TIMEOUT + " ms per try\n!!!"));
     }
 
 
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         fixture.setAssertTimeout(ASSERT_TIMEOUT);
         fixture.setMaxTryBeforeFailure(MAX_TRY_BEFORE_FAILURE);
     }
